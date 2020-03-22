8ae01f49dc723d3a45a57cfc0a29bc7cc9b00c61
Egor Sidorov <egor.sidorov@elecard.ru>
2020-03-22 03:33:03 -0400

    Done with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :validatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles
   before_save {self.email = email.downcase}
-  #validates :username, presence:true, length:{minimum: 2, maximum:20}, uniqueness: {case_sensitive: false}
+  validates :username, presence:true, length:{minimum: 2, maximum:20}, uniqueness: {case_sensitive: false}, on: :create
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true , length:{maximum:100}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
 
